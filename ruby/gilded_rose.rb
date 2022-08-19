6937c636dfd4ce23d5606770fc7b6456385c0ef0
Daniel Preciado <fpreciado3@ucol.mx>
2022-08-19 18:48:02 -0500

    Create methods for each condition to update the items quality
    - Create quality_above_zero to verify the items quality doesn't go over zero
    - Create update_quality2_condition to change the items quality according to if they're a backstage pass or not
    - Create update_quality2_conition1_quality_above_zero to reduce each item quality by 1 if the item isn't "Sulfuras, Hand of Ragnaros"
    - Reduce every pending method to 5 lines or less each
    
    Co-authored-by: JorgeRodz <jarf4231@gmail.com>
    Co-authored-by: Dcastanedaan <davinsonbmx@gmail.com>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -13,6 +13,8 @@ class GildedRose
 
   # - "Aged Brie" actually increases in Quality the older it gets
 	# - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+
+  #method_test1 se asegura de bajar la calidad de los items que no sean el item sulfuras
   def method_test1(item)
     if item.quality > 0
       if item.name != "Sulfuras, Hand of Ragnaros"
@@ -21,6 +23,7 @@ class GildedRose
     end
   end
 
+  # method_test2 se asegura de subir la calidad de los pases a backstage acorde a las condiciones
   def method_test2(item)
     item.quality = item.quality + 1
       if item.name == "Backstage passes to a TAFKAL80ETC concert"
@@ -31,6 +34,7 @@ class GildedRose
   end
 
   # quality_under_50? verifica que la calidad sea menor a 50
+  # CHANGE PENDING
   def quality_under_50?(item)
     item.quality < 50
   end
@@ -40,32 +44,63 @@ class GildedRose
     item.sell_in < sell_in
   end
 
+  # update_quality2 
+  def update_quality2(item)
+    if item.name != "Aged Brie"
+      update_quality2_condition1(item)
+    else
+      item.quality = item.quality + 1 if quality_under_50?(item)
+    end
+  end
+
+  def update_quality2_condition1(item)
+    if item.name != "Backstage passes to a TAFKAL80ETC concert"
+      quality_above_zero?(item) ? update_quality2_condition1_quality_above_zero(item) : nil
+    else
+      item.quality = item.quality - item.quality
+    end
+  end
+
+  def update_quality2_condition1_quality_above_zero(item)
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.quality = item.quality - 1
+    end
+  end
+
+  # CHANGE PENDING
+  def quality_above_zero?(item)
+    item.quality > 0
+  end
+
+  # verifica que los items se mantengan debajo de 50 en calidad y encima de 0
+  def condition1(item)
+    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+     method_test1(item)
+   else
+      method_test2(item) if quality_under_50?(item)
+   end
+  end
+
+  # verifica que los items que no sean "sulfuras" reduzcan su fecha de sell-in en 1
+  def condition2(item)
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in = item.sell_in - 1
+    end
+  end
+
+  # verifican que si un item tiene su fecha de sell_in debajo de 0 se actualice su calidad acorde a qué tipo de item es
+  def condition3(item)
+    sell_in_under_to?(item,0) ?
+       update_quality2(item)
+      : nil
+  end
+
+  #update_quality actualiza la calidad para los items registrados 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        method_test1(item)
-      else
-        method_test2(item) if quality_under_50?(item)
-      end
-
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      sell_in_under_to?(item,0) ?
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          item.quality = item.quality + 1 if quality_under_50?(item)
-        end
-      : nil
+      condition1(item)
+      condition2(item)
+      condition3(item)
     end
   end
 end
