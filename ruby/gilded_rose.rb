8d291ed1c50ceed0eca5d7edd76c0638262f50f4
Davinson Castañeda <davinsonbmx@gmail.com>
2022-08-22 18:20:58 -0500

    Rename methods name and create different_aged_brie
    co-authored-by: JorgeRodz <jarf4321@gmail.com>
    co-authored-by: fpreciado3 <fpreciado3@ucol.mx>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -19,19 +19,14 @@ class GildedRose
   # method_test1 se asegura de bajar la calidad de los items que no sean el item sulfuras
   def lower_normal_item_quality(item) # ✅
     item.quality -= 1 if quality_item_50_0(item, 0) && different_to_sulfuras?(item.name)
-    # if item.quality > 0
-    #   if item.name != "Sulfuras, Hand of Ragnaros"
-    #     item.quality = item.quality - 1
-    #   end
-    # end
   end
 
+  # different_to_sulfuras? verifica que el item no sea sulfuras
   def different_to_sulfuras?(name) # ✅
     name != 'Sulfuras, Hand of Ragnaros'
   end
 
   # quality_under_50? verifica que la calidad sea menor a 50
-  # CHANGE PENDING
   def quality_item_50_0(item, _param) # ✅
     return item.quality < 50 if _param == 50
 
@@ -43,45 +38,47 @@ class GildedRose
     item.quality += 1
     if item.name == 'Backstage passes to a TAFKAL80ETC concert'
       # verifica que el sell_in sea menor a 11 y 6 para luego verificar la calidad del item
-      sell_in_under_to?(item, 11) ? (item.quality = item.quality + 1 if quality_item_50_0(item, 50)) : nil
-      sell_in_under_to?(item, 6) ? (item.quality = item.quality + 1 if quality_item_50_0(item, 50)) : nil
+      sell_in_under_to?(item, 11) ? (item.quality += 1 if quality_item_50_0(item, 50)) : nil
+      sell_in_under_to?(item, 6) ? (item.quality +=  1 if quality_item_50_0(item, 50)) : nil
     end
   end
 
-  # CHANGE PENDING
-  # def quality_above_zero?(item)
-  #   item.quality > 0
-  # end
-
   # sell_in_under_to? verifica que sell_in sea menor a sell_in de condición
-  def sell_in_under_to?(item, sell_in)
+  def sell_in_under_to?(item, sell_in) # ✅
     item.sell_in < sell_in
   end
 
-  # update_quality2
-  def update_quality2(item)
-    if item.name != 'Aged Brie'
-      update_quality2_condition1(item)
+  # revisa que el item no sea Aged Brie
+  def different_aged_brie?(item)# ✅
+    item != 'Aged Brie'
+  end
+
+  # not_aged_brie_sellin_0 compara que no sea Aged Brie o suma calidad en caso contrario
+  def not_aged_brie_sellin_0(item) # ✅
+    if different_aged_brie?(item.name)
+      update_quality_not_backstage_sulfuras(item)
     elsif quality_item_50_0(item, 50)
-      item.quality = item.quality + 1
+      item.quality += 1
     end
   end
 
-  def update_quality2_condition1(item)
+  # los item que sean sulfuras y cumpla las condiciones de los metodos anteriores, le reduce la calidad en 1
+  def update_quality_not_backstage_sulfuras(item) # ✅
     if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      quality_item_50_0(item, 0) ? update_quality2_condition1_quality_above_zero(item) : nil
+      quality_item_50_0(item, 0) ? item_lower_quality_not_sulfuras(item) : nil
     else
-      item.quality = item.quality - item.quality
+      item.quality -= item.quality
     end
   end
 
-  def update_quality2_condition1_quality_above_zero(item)
-    item.quality = item.quality - 1 if different_to_sulfuras?(item.name)
+  # reduce la calidad en 1 si cumple las condiciones anteriores al metodo y
+  def item_lower_quality_not_sulfuras(item) # ✅
+    item.quality -= 1 if different_to_sulfuras?(item.name)
   end
 
   # verifica que los items se mantengan debajo de 50 en calidad y encima de 0
-  def condition1(item)
-    if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
+  def item_quality_under_50_above_0(item) # ✅
+    if different_aged_brie?(item.name) && item.name != 'Backstage passes to a TAFKAL80ETC concert'
       lower_normal_item_quality(item)
     elsif quality_item_50_0(item, 50)
       backstage_passes_quality(item)
@@ -89,21 +86,21 @@ class GildedRose
   end
 
   # verifica que los items que no sean "sulfuras" reduzcan su fecha de sell-in en 1
-  def condition2(item)
-    item.sell_in = item.sell_in - 1 if different_to_sulfuras?(item.name)
+  def not_sulfuras(item) # ✅
+    item.sell_in -= 1 if different_to_sulfuras?(item.name)
   end
 
   # verifican que si un item tiene su fecha de sell_in debajo de 0 se actualice su calidad acorde a qué tipo de item es
-  def condition3(item)
-    update_quality2(item) if sell_in_under_to?(item, 0)
+  def sell_in_above_0(item) # ✅
+    not_aged_brie_sellin_0(item) if sell_in_under_to?(item, 0)
   end
 
   # update_quality actualiza la calidad para los items registrados
-  def update_quality
+  def update_quality # ✅
     @items.each do |item|
-      condition1(item)
-      condition2(item)
-      condition3(item)
+      item_quality_under_50_above_0(item)# condicion1
+      not_sulfuras(item)# condition2
+      sell_in_above_0(item)# condition
     end
   end
 end
